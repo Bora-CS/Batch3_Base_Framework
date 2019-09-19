fda63f56cbc2c56871b16d3a94d657244d8566a3
Amar Zahed <Amarzahed@gmail.com>
2019-09-19 17:23:37 -0400

    Homeworks are done.

---
+++ src/test/java/SeleniumPractices/CostcoWaitTest.java
@@ -0,0 +1,53 @@
+package SeleniumPractices;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.boratech.util.Keywords;
+
+public class CostcoWaitTest {
+	public static void main(String[] args) {
+		WebDriver driver = Keywords.startTestOnChrome("https://www.costco.com/");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
+				"//a[@href='/CatalogSearch?dept=All&keyword=appleseptmvm19'][@role='tabpanel'][@aria-hidden='false']")));
+		driver.findElement(By.id("hero-carousel")).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href,'100497427')]")));
+		String appleText = driver.findElement(By.id("rsltCntMsg")).getText();
+		if (appleText.contains("apple")) {
+			System.out.println("Test passed! = " + appleText);
+		}
+
+		driver.findElement(By.xpath("//a[contains(@href,\"100497427\")]")).click();
+		String itemVerify = driver.findElement(By.xpath("//span[@itemprop='sku']")).getAttribute("data-sku");
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-btn")));
+		driver.findElement(By.id("add-to-cart-btn")).click();
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn.btn-primary.btn-block")));
+		driver.findElement(By.cssSelector(".btn.btn-primary.btn-block")).click();
+
+		String cartItemNumber = driver.findElement(By.xpath("//div[@class='order-item']"))
+				.getAttribute("data-orderitemnumber");
+		if (cartItemNumber.equals(itemVerify)) {
+			System.out.println("Test passed = " + "Cart Item No =" + cartItemNumber + " Item Verify = " + itemVerify);
+		} else {
+			System.out.println("Test failed!" + "Cart Item No =" + cartItemNumber + " Item Verify = " + itemVerify);
+		}
+
+		try {
+			driver.findElement(By.id("shopCartCheckoutSubmitButton"));
+			System.out.println("Checkout button found!");
+		} catch (NoSuchElementException e) {
+			System.out.println("Checktout button could not be found!");
+		}
+
+		Keywords.endTest(driver);
+
+	}
+}
